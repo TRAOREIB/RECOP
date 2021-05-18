b987cf0efcdd9489d7e505c9179fcdff760c6613
Dicko13 <montestk21@gmail.com>
2021-05-18 13:58:03 +0000

    ajout de fichiers

---
+++ resources/views/candidat/recherchenational.blade.php
@@ -0,0 +1,91 @@
+{{-- @extends('template')
+@section('contenu')
+@section('script') --}}
+
+<script src="{{asset('js/bootstrap.min.js')}}"></script>
+<script src="{{asset('js/jquery.min.js')}}"></script>
+<script src="{{asset('js/datatables.min.js')}}"></script>
+<link rel="stylesheet" href="{{asset('css/datatables.min.css')}}">
+<link rel="stylesheet" href="{{asset('css/bootstrap.css')}}"> 
+{{-- @endsection --}}
+<div class="row listetitreformgrand" style="">
+      <div class="col-sm-12" style="">
+        <h3 class=" offset-sm-2"> CORRESPONDANTS NATIONAUX </h3>
+      </div>
+</div> <br>
+
+<div class="row  offset-sm-2">
+   
+       <label for="region" class="">REGION </label> 
+        <div class="col-sm-2  col-md-2 col-xs-6"> 
+            <select class="form-control" name="region" required> 
+                <option selected></option>                                    
+                <option value=""></option>                                    
+                <small class="text-danger">{{ $errors->first('region',':message') }}</small> 
+            </select> 
+        </div>
+
+    <div class="ln_solid "></div>
+       <div class="item form-group">
+           <div style="margin: auto">  
+               <input type="submit"  value="VALIDER" class="btn btn-primary" style="border: 1px solid #FFFFFF;  border-radius: 1px;margin-top: 0px"> 
+           </div>
+    </div>
+</div>
+
+<div class="row divtabgrand  offset-sm-1" style="">
+    <div class="col-xs-12" >
+        <div class="col-xs-12 ligneform " style="background-color: #EEE">
+            <table class="table table-striped table-condensed" id="table">
+                <thead>
+                    <tr style="background-color:#2a6496;color: #FFFFFF;">
+                        <th>NOM</th>
+                        <th>PRENOM(S)</th>
+                        <th>TELEPHONE</th>
+                        <th>MAIL</th>
+                        <th>PRESSE</th>
+                        <th>LIEU DE RESIDENCE</th>
+                       
+                    </tr>
+                </thead>
+                <tbody>
+
+                    
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+              
+                    </tr>
+                    
+
+                </tbody>
+            </table>
+
+        </div>
+    </div>
+</div>
+<!--  fin de la liste-->
+
+<div class="modal" id="confirmationta">
+    <div class="modal-dialog modal-sm">
+        <div class="modal-content" style="background-color : whitesmoke">
+            <div class="modal-body">
+                <span class="glyphicon glyphicon-exclamation-sign" style="color: #f0ad4e"><label > Supprimer cet &eacutelement ? </label></span>
+            </div>
+            <div class="modal-footer">
+                <a href="#"  class="btn btn-success" data-dismiss="modal"><span class="glyphicon glyphicon-ok-sign"></span> OUI</a>
+                <a href="#"  class="btn btn-danger" data-dismiss="modal"><span class="glyphicon glyphicon-remove-sign"></span> NON</a>
+            </div>
+        </div>
+    </div>
+</div>
+<script>
+    $(document).ready(function () {
+        $('#table').DataTable();
+    });
+</script>
+{{-- @endsection --}}
